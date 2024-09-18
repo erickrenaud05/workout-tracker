1e3bb5979e77f873461502f55683c47b1e67e15b
Erick Renaud <renaud_junior@outlook.com>
2024-09-18 19:24:32 -0400

    Added bcrypt and password hashing
    User schema now has hashing hook using bcrypt and checkpassword methos

--- backEnd/config/connection.js
+++ backEnd/config/connection.js
@@ -1,6 +1,6 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/limitless'; 
+const mongoURI = 'mongodb://127.0.0.1:27017/limitless'; 
 
 const connectDB = async () => {
     try {
