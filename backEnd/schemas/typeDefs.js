44f88dd0ada7be4de18aa28acd7bf6b462e270e6
Erick Renaud <renaud_junior@outlook.com>
2024-09-19 19:29:43 -0400

    Added exercise to type def

--- backEnd/schemas/typeDefs.js
+++ backEnd/schemas/typeDefs.js
@@ -9,6 +9,13 @@ const typeDefs = `
     type Workout {
         day: String
         name: String
+        exercises: [Exercise]
+    }
+
+    type Exercise {
+      name: String
+      reps: Int
+      sets: Int
     }
 
   # Define which queries the front end is allowed to make and what data is returned
