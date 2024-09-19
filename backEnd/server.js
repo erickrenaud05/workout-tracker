7659060b03e480b034373e5b289155b8c45f9b5d
Erick Renaud <renaud_junior@outlook.com>
2024-09-19 18:51:06 -0400

    Added graphql apollo server

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,56 +1,33 @@
 const express = require('express');
 const connectDB = require('./config/connection');
-const {User} = require('./model');
-const workoutSchema = require('./model/workout');
-connectDB();
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { typeDefs, resolvers } = require('./schemas');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 const app = express();
 
 const PORT =  process.env.PORT || 3001;
 
-app.post('/', async(req, res)=>{
-    // const newUser = new User({
-    //     name: 'Erick',
-    //     username: 'COol',
-    //     password: 'password'
-    // });
-
-    // await newUser.save();
-    // res.status(200).json(newUser);
-
-    // const user = await User.findOne({ name: 'Erick'});
+const startApolloServer = async () => {
+    await server.start();
     
-    // if(user.checkPassword('password')){
-    //     res.status(200).json(user)
-    // } else{
-    //     res.status(401).json('uhoh')
-    // }
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+  
+    app.use('/graphql', expressMiddleware(server));
+  
+    connectDB();
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
 
-    //Create with workout
-            // const response = await new User({
-        //     name: 'Erick',
-        //     username: 'Myuser',
-        //     password: 'password',
-        //     workout: [{
-        //         day: 'Monday',
-        //         name: 'ChestDay',
-        //         exercise: [
-        //             {
-        //                 name: 'ChestPress',
-        //                 reps: 2,
-        //                 sets: 3,
-        //             },
-        //             {
-        //                 name: 'FLies',
-        //                 reps: 4,
-        //                 sets: 5
-        //             }
-        //         ]
-        //     }]
-        // });
+};
 
-})
-
-app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-});
+  
+startApolloServer();
