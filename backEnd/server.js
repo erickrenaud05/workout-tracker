c0d14f98de65bf823ac96f507a3306df289fae16
Erick Renaud <renaud_junior@outlook.com>
2024-09-30 18:38:17 -0400

    Added fixes

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/connection');
-
+const path = require('path');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
